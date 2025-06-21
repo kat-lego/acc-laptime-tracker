d1d8b0f5d6c61f5a9b8f73d599544e33dc967161
katlego <katlegomodupi@protonmail.com>
2025-06-21 13:43:18 +0200

    delete old infra files

--- infra-up.sh
+++
@@ -1,7 +0,0 @@
-
-az deployment group create \
-    --resource-group rg-acc-laptime-tracker \
-    --template-file ./main.bicep \
-    --parameters \
-        cosmosDbConnectionString="$(pass cosmos/acclaptracker)" \
-        githubToken="$(pass acc_github_pat)"
